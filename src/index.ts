03e95032d4ed5f4cf752a9245b64861fd06694c9
Matt Beech <matthew.beech@moonpig.com>
2021-07-01 17:37:28 +0100

    feat: add mongo connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { ApolloServer, makeExecutableSchema } from 'apollo-server';
 import { config } from 'dotenv';
+import mongoose from 'mongoose';
 import { resolvers } from './resolvers';
+import { logger } from './shared';
 import { typeDefs } from './typeDefs';
 
 config();
@@ -11,8 +13,23 @@ const schema = makeExecutableSchema({
   resolverValidationOptions: { requireResolversForResolveType: false },
 });
 
+mongoose
+  .connect(process.env.DATABASE_CONNECTION_STRING ?? '', {
+    auth: {
+      user: process.env.DATABASE_USER ?? '',
+      password: process.env.DATABASE_PASSWORD ?? '',
+    },
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    logger.info('Connected to database');
+  });
+
 const server = new ApolloServer({ schema });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server
+  .listen({ port: process.env.PORT || 4000, cors: false })
+  .then(({ url }) => {
+    logger.info(`🚀  Server ready at ${url}`);
+  });
