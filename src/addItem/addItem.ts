2b3e3cd6cdc76170215cd230ee05843b9889ea2d
Matt Beech <matthew_beech@live.co.uk>
2021-08-13 00:30:45 +0100

    fix: surround create item in try catch

--- src/addItem/addItem.ts
+++ src/addItem/addItem.ts
@@ -5,27 +5,27 @@ export const addItem: MutationResolvers['addItem'] = async (
   { campaignId, input: { tags, quantity, ...input } },
   { prisma },
 ): Promise<AddItemResult> => {
-  const { campaign } = await prisma.item.create({
-    data: {
-      campaignId,
-      ...input,
-      quantity: quantity ?? undefined,
-      tags: tags ?? undefined,
-    },
-    include: { campaign: true },
-  });
+  try {
+    const { campaign } = await prisma.item.create({
+      data: {
+        campaignId,
+        ...input,
+        quantity: quantity ?? undefined,
+        tags: tags ?? undefined,
+      },
+      include: { campaign: true },
+    });
 
-  if (!campaign) {
+    logger.info(`Added item with to campaign with ID ${campaignId}`);
+
+    return {
+      __typename: 'Campaign',
+      ...campaign,
+    };
+  } catch {
     return {
       __typename: 'CampaignNotFound',
       message: `Campaign with ID ${campaignId} does not exist`,
     };
   }
-
-  logger.info(`Added item with to campaign with ID ${campaignId}`);
-
-  return {
-    __typename: 'Campaign',
-    ...campaign,
-  };
 };
