dca2e2a6604ff02a4e2ca3f764d71b264bc470b0
Matt Beech <matthew_beech@live.co.uk>
2021-08-12 22:43:39 +0100

    feat: use try catches on database operations

--- src/removeTag/removeTag.ts
+++ src/removeTag/removeTag.ts
@@ -5,23 +5,26 @@ export const removeTag: MutationResolvers['addTag'] = async (
   { itemId, tag },
   { prisma },
 ): Promise<AddTagResult> => {
-  const item = await prisma.item.findUnique({ where: { id: itemId } });
+  try {
+    const item = await prisma.item.findUnique({
+      where: { id: itemId },
+      rejectOnNotFound: true,
+    });
 
-  if (!item) {
+    const newTags = item.tags.filter((t) => t !== tag);
+
+    const updatedItem = await prisma.item.update({
+      where: { id: itemId },
+      data: { tags: { set: newTags } },
+    });
+
+    logger.info(`Removed tag from item ${itemId}`);
+
+    return { __typename: 'Item', ...updatedItem };
+  } catch {
     return {
       __typename: 'ItemNotFound',
       message: `Item with ID ${itemId} not found`,
     };
   }
-
-  const newTags = item.tags.filter((t) => t !== tag);
-
-  const updatedItem = await prisma.item.update({
-    where: { id: itemId },
-    data: { tags: { set: newTags } },
-  });
-
-  logger.info(`Removed tag from item ${itemId}`);
-
-  return { __typename: 'Item', ...updatedItem };
 };
