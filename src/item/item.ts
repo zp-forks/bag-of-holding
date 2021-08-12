c6c1f3314ab7e33bfa12dddd8de3dd529568b37d
Matt Beech <matthew_beech@live.co.uk>
2021-08-13 00:52:09 +0100

    feat: add item resolver

---
+++ src/item/item.ts
@@ -0,0 +1,26 @@
+import { FetchItemResult, logger, QueryResolvers } from '../shared';
+
+export const fetchItem: QueryResolvers['item'] = async (
+  _,
+  { itemId },
+  { prisma },
+): Promise<FetchItemResult> => {
+  try {
+    const item = await prisma.item.findUnique({
+      where: { id: itemId },
+      rejectOnNotFound: true,
+    });
+
+    logger.info(`Fetched item ${itemId}`);
+
+    return {
+      __typename: 'Item',
+      ...item,
+    };
+  } catch {
+    return {
+      __typename: 'ItemNotFound',
+      message: `Item with ID ${itemId} does not exist`,
+    };
+  }
+};
